fff1162d5ca2563245eff33c7f2c375ac8e41fa5
Brady Guo <brady_guo@brogent.com>
2024-11-28 17:45:51 +0800

    Add game process

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,17 +2,40 @@ extends Control
 
 const MEMORY_TILE = preload("res://scenes/memory_tile/memory_tile.tscn")
 # Called when the node enters the scene tree for the first time.
+@onready var tiles_container: GridContainer = $HB/MarginContainer/TilesContainer
+@onready var scorer = $Scorer
+@onready var label_moves: Label = $HB/MarginContainer2/VBoxContainer/HBoxContainer/LabelMoves
+@onready var label_pairs: Label = $HB/MarginContainer2/VBoxContainer/HBoxContainer2/LabelPairs
+
+
 func _ready() -> void:
 	SignalManager.on_level_selected.connect(on_level_selected)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	label_moves.text = scorer.get_moves_made_string()
+	label_pairs.text = scorer.get_pairs_made_string()
+	
+	
+func add_memory_tile(item_image: ItemImage, frame: Texture2D) -> void:
+	var new_tile: MemoryTile = MEMORY_TILE.instantiate()
+	tiles_container.add_child(new_tile)
+	new_tile.setup(item_image, frame)
+	
+	
 	
 func on_level_selected(level_num: int) -> void:
-	print(level_num)
+	var level_data: SelectedLevelData = GameManager.get_level_selection(level_num)
+	var frame: Texture2D = ImageManager.get_random_frame_image()
+	
+	tiles_container.columns = level_data.get_num_cols()
+	for image in level_data.get_selected_level_image():
+		add_memory_tile(image, frame)
+	scorer.clear_new_game(level_data.get_target_pairs())
+	
 
 
 func _on_exit_button_pressed() -> void:
+	for tile in tiles_container.get_children():
+		tile.queue_free()
 	SignalManager.on_game_exit_pressed.emit()
---
+++ scenes/scorer/scorer.gd
@@ -0,0 +1,66 @@
+extends Node
+
+@onready var reveal_timer: Timer = $RevealTimer
+
+var _selections: Array[MemoryTile] = []
+var _target_pairs: int = 0
+var _moves_made: int = 0
+var _pairs_made: int = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalManager.on_tile_selected.connect(on_tile_selected)
+	SignalManager.on_game_exit_pressed.connect(on_game_exit_pressed)
+
+func on_tile_selected(tile: MemoryTile) -> void:
+	tile.reveal_image(true)
+	_selections.append(tile)
+	check_pair_made()
+	
+func selection_are_pair() -> bool:
+	return _selections[0].matches_other_tile(_selections[1])
+	
+func check_pair_made() -> void:
+	if _selections.size() != 2:
+		return
+
+	reveal_timer.start()
+	SignalManager.on_selection_disabled.emit()
+	_moves_made += 1
+	
+	
+	if selection_are_pair() == true:
+		kill_tiles()
+	
+func  kill_tiles() -> void:   
+	for tile in _selections:
+		tile.kill_on_success()
+		
+	_pairs_made += 1
+	
+
+func clear_new_game(target_pairs: int) ->void:
+	_selections.clear()
+	_target_pairs = target_pairs
+	_moves_made = 0
+	_pairs_made = 0
+
+func on_game_exit_pressed() -> void:
+	reveal_timer.stop()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_pairs_made_string() -> String:
+	return "%d/%d" % [_pairs_made, _target_pairs]
+
+func get_moves_made_string() -> String:
+	return str(_moves_made)
+
+func _on_reveal_timer_timeout() -> void:
+	if selection_are_pair() == false:
+		for tile in _selections:
+			tile.reveal_image(false)
+	_selections.clear()
+	
+	SignalManager.on_selection_enabled.emit()
